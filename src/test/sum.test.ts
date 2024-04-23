ffbb5d2320f0e1646e57e597c4b75b0ce24553d2
SamuelAroca <samuel.arocad@unac.edu.co>
2024-04-23 15:52:19 -0500

    Sum test 4 y 5

--- src/test/sum.test.ts
+++ src/test/sum.test.ts
@@ -27,4 +27,22 @@ describe("Sum of numbers", () => {
 
     expect(result).toBeLessThanOrEqual(8);
   });
+
+  test('The sum of two values is less than 12', () => {
+    const a = 5;
+    const b = 6;
+
+    const result = sum(a, b);
+
+    expect(result).toBeLessThan(12);
+  });
+
+  test('The sum of two values is equal to 9', () => {
+    const a = 4;
+    const b = 5;
+
+    const result = sum(a, b);
+
+    expect(result).toEqual(9);
+  });
 });
