ab7f26126c57c0ade31d4ff769cee6801dfc0165
SamuelAroca <samuel.arocad@unac.edu.co>
2024-04-23 15:26:35 -0500

    String Test 3

--- src/test/string.test.ts
+++ src/test/string.test.ts
@@ -1,11 +1,14 @@
-import { anything } from "../string";
+import { anything, anything1 } from "../string";
 
-describe('Strings', () => {
+describe("Strings", () => {
   test('anything should contain "UN"', () => {
     expect(anything).toContain("UN");
   });
 
   test('anything should not contain "P"', () => {
     expect(anything).not.toContain("P");
+  });
+  test('anything1 should contain the letter "n"', () => {
+    expect(anything1).toContain("n");
   });
 });
