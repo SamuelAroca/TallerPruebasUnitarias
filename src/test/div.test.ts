8e72a4897917a814150efc850010ee90f250e97c
SamuelAroca <samuel.arocad@unac.edu.co>
2024-04-23 15:05:40 -0500

    Divisions test 2

--- src/test/div.test.ts
+++ src/test/div.test.ts
@@ -1,12 +1,21 @@
-import { div } from '../div';
+import { div } from "../div";
 
-describe('Division test', () => {
-  test('Correct division', () => {
+describe("Division test", () => {
+  test("Correct division", () => {
     const a = 10;
     const b = 2;
 
     const result = div(a, b);
 
     expect(result).toBe(5);
+  });
+
+  test("Division by zero", () => {
+    const a = 10;
+    const b = 0;
+
+    const result = div(a, b);
+
+    expect(result).toBe(Infinity);
   });
 });
