382bb3123fb7189966611cc6e7496a4d774cef77
JimDiazC <101286666+JimDiazC@users.noreply.github.com>
2024-04-23 15:16:26 -0500

    Multi test number 2 and 3

--- src/test/multi.test.ts
+++ src/test/multi.test.ts
@@ -1,7 +1,16 @@
-import { n } from "../multi";
+import { n, number } from "../multi";
 
 describe("Multi test", () => {
   test("n should be null", () => {
     expect(n).toBe(null);
   });
+
+  test("n should be defined (not is undefined)", () => {
+    expect(n).toBeDefined();
+  });
+
+  test("number should be 0", () => {
+    expect(number).toBe(0);
+  });
+
 });
